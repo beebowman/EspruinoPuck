5701b26dca2400c6678a11a9d23ac0132f45c795
beebowman <78465864+beebowman@users.noreply.github.com>
2023-07-09 22:52:11 -0400

    Add files via upload

---
+++ RPi Puck Version 4.js
@@ -0,0 +1,29 @@
+//Instructions at:https://core-electronics.com.au/guides/using-usb-and-bluetooth-controllers-with-python
+
+var kb = require("ble_hid_keyboard");
+NRF.setServices(undefined, { hid : kb.report });
+var reset_timer;
+var next = "b"; //this is the letter that goes to the terminal 
+
+function sendNext(){
+  sendCharNext();
+  LED2.set();
+  setTimeout("LED2.reset()",1000);
+}
+
+function sendCharNext(){
+        if (next == next.toLowerCase()){
+            sk = 0;
+        } else {
+            sk = 0x02;
+        }
+        // send the "n" keyboard character
+        kb.tap(kb.KEY[next.toUpperCase()], sk);
+}
+
+setWatch(function(e) {
+  if(e.time-e.lastTime < 0.5){ // on short press slide goes one step forward
+    sendNext();
+  }
+}, BTN, {edge:"falling", debounce:50, repeat:true});
+
